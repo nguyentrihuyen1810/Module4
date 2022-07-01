a71ab0764fa5cf95af82f45a22c19585e9f341ed
nguyentrihuyen1810 <93263209+nguyentrihuyen1810@users.noreply.github.com>
2022-07-01 18:54:27 +0700

    push bài 10

---
+++ b10. Session_and_Cookie/Hoan_thien_bai_gio_hang/src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String image;
+    private double price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String image, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
